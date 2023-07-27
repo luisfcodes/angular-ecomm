c20cde9792648e9d20a992a8c2a2a1b498ed7431
Luis Fernando <luis.silva9902@gmail.com>
2023-07-27 19:24:10 -0300

    Creation of the Angular Standalone project with NX (Powerful open-source build system), library creation and creation of a Standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomeComponent } from '@angular-ecomm/home'
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HomeComponent],
   selector: 'angular-ecomm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
