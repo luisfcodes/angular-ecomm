7e8ef5c3d46a4f5280499fef0c1fd26f5de94a37
Luis Fernando <luis.silva9902@gmail.com>
2023-07-28 17:57:23 -0300

    Created Angular monorepo with Nx, created home library and installed angular material

---
+++ Nx + Angular + NgRx (E-commerce App)/org/apps/luisfcodes-workspace/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent, RouterModule, MainNavComponent],
+  selector: 'org-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'luisfcodes-workspace';
+}
